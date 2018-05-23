29b123c8ffd6dd9348588acf37d620f65dba4b70
Hami Kaveii <hamidreza.kaveii@gmail.com>
2018-05-23 09:08:43 -0400

    Lab creation

--- src/refactoring/Lab.java
+++ src/refactoring/Lab.java
@@ -10,5 +10,34 @@ package refactoring;
  * @author 1795545
  */
 public class Lab {
+    String idClass;
+    Integer capacity;
+    Integer nbComputers;
+
+    public String getIdClass() {
+        return idClass;
+    }
+
+    public void setIdClass(String idClass) {
+        this.idClass = idClass;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getNbComputers() {
+        return nbComputers;
+    }
+
+    public void setNbComputers(Integer nbComputers) {
+        this.nbComputers = nbComputers;
+    }
+    
+    
     
 }
